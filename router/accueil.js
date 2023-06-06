a8bbae0c07c770a98b3b1ce825df9af49d38834e
Drenore <benjamin.cristinelli1@gmail.com>
2023-06-06 14:46:29 +0200

    Remove useless doc

--- router/accueil.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const {pageAccueil} = require('../controller/index')
-const index = express.Router()
-
-index.get('/', pageAccueil)
-
-module.exports = index;
--- serverExpress.js
+++ serverExpress.js
@@ -1,15 +1,11 @@
 const express = require('express')
-const index = require('./router/accueil')
+
 
 const app = express()
 
-app.set('views', './vue')
-app.set('view engine',  'ejs')
-
 
 app.use('/', index)
-app.use(express.static('static'))
-app.use(express.urlencoded({extended : true}))
+app.use(express.json())
 
 
 app.listen(3000)
